80a0930baf3c0977b971b5a600b92c748200a00b
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2022-11-12 09:32:23 +0000

    tochuuBookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'books'=>'books#index'
+  get 'books/:id'=>'books#show'
+  get 'books/:id'=>'books#edit'
+  get root :to =>'homes#top'
+
+  #resources:books
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
