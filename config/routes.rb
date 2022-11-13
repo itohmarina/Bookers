fa7faf6fa0707c4feefb3ff4e3069714d38bd1ea
ItohMarina <117239917+itohmarina@users.noreply.github.com>
2022-11-13 09:13:40 +0000

    addsomeRouting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :books
+
   get 'books'=>'books#index'
   get 'books/:id'=>'books#show'
-  get 'books/:id'=>'books#edit'
+  get 'books/:id/edit'=>'books#edit'
   get root :to =>'homes#top'
+  post 'books/:id'=>'books#update'
+  delete 'books'=>'books#destroy', as:'destroy_book'
 
-  #resources:books
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
